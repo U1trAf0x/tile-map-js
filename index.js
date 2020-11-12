5869293751575081d6c2698a8734bb4aa799ebc1
U1trAf0x <ultrafox2016@gmail.com>
2020-11-12 15:27:59 +0300

    new

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#f00"; // flex
+ctx.fillStyle = "#f00"; // flex // flex
 ctx.fillRect(0, 0, 300, 300);
